1235a1c034d5c08b801b36cd563110604a0a1b53
NicolasBuscemi <91195492+NicolasBuscemi@users.noreply.github.com>
2023-05-30 13:22:59 +0200

    Working

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
-"use strict";
+'use strict';
 
 const app = {
-    map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
+    map: null, // use this variable to reference the map throughout the application
+
     init() {
-        // initialise de kaart
+        // initialize the map
+        this.map = L.map('map').setView([50.846475, 4.352793], 13);
 
-        // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-        // vergeet openstreetmap attributie niet
-        
-        // gebruik de functie "loadMarkers" om de markers toe te voegen
+        // add a tile layer using the OpenStreetMap tile URL
+        L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        }).addTo(this.map);
+
+        // use the function "loadMarkers" to add markers to the map
+        this.loadMarkers();
     },
+
     loadMarkers() {
+        // fetch the data from opendata.brussels.be
+        fetch('https://opendata.brussels.be/api/records/1.0/search/?dataset=toiletten&q=&rows=100&geofilter.distance=50.846475%2C+4.352793%2C+5000')
+            .then((response) => response.json())
+            .then((data) => {
+                // loop over the items
+                data.records.forEach((item) => {
+                    // check if coordinates are available
+                    if (item.geometry && item.geometry.coordinates) {
+                        const lat = item.geometry.coordinates[1];
+                        const lon = item.geometry.coordinates[0];
 
-        // fetch de data van opendata.brussels.be
-            // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
-        
-        
+                        // use the addMarker function to add a marker to the map
+                        this.addMarker(lat, lon);
+                    }
+                });
+            });
     },
+
     addMarker(lat, lon) {
-        // voeg een marker toe op lat, lon
-    }
-}
+        // create a marker at the specified lat, lon coordinates
+        L.marker([lat, lon]).addTo(this.map)
+            .bindPopup('Toilet location'); // add a popup that displays the location of the toilet
+    },
+};
 
 app.init();
